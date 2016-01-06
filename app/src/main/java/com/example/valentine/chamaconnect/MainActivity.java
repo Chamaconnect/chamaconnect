a6f21c70045ef076264e8342422b7359376f18f1
valentinerutto <rutto28@gmail.com>
2016-01-06 12:46:22 +0300

    initialized parse keys on myapp

--- app/src/main/java/com/example/valentine/chamaconnect/MainActivity.java
+++ app/src/main/java/com/example/valentine/chamaconnect/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.valentine.chamaconnect;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.parse.ParseAnonymousUtils;
-import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,30 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Determine whether the current user is an anonymous user
-        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-            // If user is anonymous, send the user to LoginSignupActivity.class
-            Intent intent = new Intent(MainActivity.this,
-                    LoginActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            // If current user is NOT anonymous user
-            // Get current user data from Parse.com
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            if (currentUser != null) {
-                // Send logged in users to Welcome.class
-                Intent intent = new Intent(MainActivity.this, Welcome.class);
-                startActivity(intent);
-                finish();
-            } else {
-                // Send user to LoginSignupActivity.class
-                Intent intent = new Intent(MainActivity.this,
-                        LoginSignupActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }
+
     }
 
     @Override
--- app/src/main/java/com/example/valentine/chamaconnect/Signup.java
+++ app/src/main/java/com/example/valentine/chamaconnect/Signup.java
@@ -1,9 +1,13 @@
 package com.example.valentine.chamaconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
 
 public class Signup extends AppCompatActivity {
 
@@ -11,6 +15,31 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Determine whether the current user is an anonymous user
+        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            // If user is anonymous, send the user to LoginSignupActivity.class
+            Intent intent = new Intent(Signup.this,
+                    LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // If current user is NOT anonymous user
+            // Get current user data from Parse.com
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            if (currentUser != null) {
+                // Send logged in users to Welcome.class
+                Intent intent = new Intent(Signup.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Send user to LoginSignupActivity.class
+                Intent intent = new Intent(Signup.this,
+                        LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 
     @Override
