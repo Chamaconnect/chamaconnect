7d06635387d207ed5c0da361f4f516faf7dc2da6
valentinerutto <rutto28@gmail.com>
2016-01-07 16:44:36 +0300

    initialized parse

--- app/src/main/java/com/example/valentine/chamaconnect/MainActivity.java
+++ app/src/main/java/com/example/valentine/chamaconnect/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.valentine.chamaconnect;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.parse.ParseAnonymousUtils;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,29 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        // Determine whether the current user is an anonymous user
-        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-            // If user is anonymous, send the user to LoginSignupActivity.class
-            Intent intent = new Intent(MainActivity.this,
-                    LoginActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            // If current user is NOT anonymous user
-            // Get current user data from Parse.com
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            if (currentUser != null) {
-                // Send logged in users to Welcome.class
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            } else {
-                // Send user to LoginSignupActivity.class
-                Intent intent = new Intent(MainActivity.this,
-                        LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }}
+//        // Determine whether the current user is an anonymous user
+//        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+//            // If user is anonymous, send the user to LoginSignupActivity.class
+//            Intent intent = new Intent(MainActivity.this,
+//                    MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        } else {
+//            // If current user is NOT anonymous user
+//            // Get current user data from Parse.com
+//            ParseUser currentUser = ParseUser.getCurrentUser();
+//            if (currentUser != null) {
+//                // Send logged in users to Welcome.class
+//                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            } else {
+//                // Send user to LoginSignupActivity.class
+//                Intent intent = new Intent(MainActivity.this,
+//                        MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }}
     }
 
     @Override
