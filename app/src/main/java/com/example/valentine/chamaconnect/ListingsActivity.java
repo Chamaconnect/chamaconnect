3508a2c0ca20fb03bfc120008834594d596da475
colleowino <colleowino@gmail.com>
2016-01-09 01:12:26 +0300

    renamed listings activity

--- app/src/main/java/com/example/valentine/chamaconnect/ListingsActivity.java
+++ app/src/main/java/com/example/valentine/chamaconnect/ListingsActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class ToolbarActivity extends AppCompatActivity {
+public class ListingsActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
@@ -29,14 +29,6 @@ public class ToolbarActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-       // ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                //R.array.counties_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        //spinner.setAdapter(adapter);
-
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.counties_array, R.layout.spinner_layout);
 // Specify the layout to use when the list of choices appears
